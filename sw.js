bc479d0e48e27806e1cebce09bf09785e715d831
edson mendoza <eamramirez95@gmail.com>
2022-02-16 13:38:28 -0500

    nuevas modificaciones

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 
 self.importScripts("js/sw-util.js");
 
-const STATIC_CACHE="static-v2";
-const DYNAMIC_CACHE="dynamic-v1";
+const STATIC_CACHE="static-v3";
+const DYNAMIC_CACHE="dynamic-v2";
 const INMUTABLE_CACHE="inmutable-v1";
 
 const APP_SHELL=["/index.html","css/style.css","img/favicon.ico","img/avatars/hulk.jpg","img/avatars/ironman.jpg","img/avatars/spiderman.jpg","img/avatars/thor.jpg","img/avatars/wolverine.jpg","js/app.js"];
@@ -36,6 +36,10 @@ self.addEventListener("activate",e=>{
     const promesa = caches.keys().then(keys=>{
         keys.forEach(key=>{
             if(key !== STATIC_CACHE && key.includes("static")){
+                return caches.delete(key);
+            }
+
+            if(key !== DYNAMIC_CACHE && key.includes("dynamic")){
                 return caches.delete(key);
             }
         });
